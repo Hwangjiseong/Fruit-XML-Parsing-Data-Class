ddac31b3c2125a8c398927c4dd9f5c3c9fa3aef9
D7703_08 <d7703_08@d7703-08ui-imac.local>
2018-10-01 15:58:59 +0900

    123

---
+++ Fruit-XML-Parsing-Data-Class/FruitData.swift
@@ -0,0 +1,14 @@
+//
+//  FruitData.swift
+//  Fruit-XML-Parsing-Data-Class
+//
+//  Created by D7703_08 on 2018. 10. 1..
+//  Copyright © 2018년 D7703_08. All rights reserved.
+//
+
+import Foundation
+class FruitData {
+    var name = ""
+    var color = ""
+    var cost = ""
+}
